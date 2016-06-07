61c094e9379e6866a47ea73be0005bbcace47472
Md Rezaul Karim <rezaul.km105@gmail.com>
2016-06-07 16:35:00 +0200

    init

--- src/main/java/com/here/ptr/PublicTransportRouting.java
+++ src/main/java/com/here/ptr/PublicTransportRouting.java
@@ -5,8 +5,17 @@
  */
 package com.here.ptr;
 
+import com.here.ptr.graph.domain.Edge;
+import com.here.ptr.graph.domain.Graph;
+import com.here.ptr.graph.domain.Node;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -15,20 +24,115 @@ import java.util.Map;
 public class PublicTransportRouting {
 
     private Map<String, Integer> transportGraph = new HashMap<String, Integer>();
+    private final List<Node> nodes;
+    private final List<Edge> edges;
+    private Set<Node> settledNodes;
+    private Set<Node> unSettledNodes;
+    private Map<Node, Node> predecessors;
+    private Map<Node, Integer> distance;
 
-    public PublicTransportRouting() {
+    public PublicTransportRouting(Graph graph) {
+        this.nodes = new ArrayList<Node>(graph.getNodes());
+        this.edges = new ArrayList<Edge>(graph.getEdges());
+    }
+
+    public void runSrearch(Node source) {
+        settledNodes = new HashSet<Node>();
+        unSettledNodes = new HashSet<Node>();
+        distance = new HashMap<Node, Integer>();
+        predecessors = new HashMap<Node, Node>();
+        distance.put(source, 0);
+        unSettledNodes.add(source);
+        while (unSettledNodes.size() > 0) {
+            Node node = getMinimum(unSettledNodes);
+            settledNodes.add(node);
+            unSettledNodes.remove(node);
+            findMinimalDistances(node);
+        }
+    }
+
+    private Node getMinimum(Set<Node> nodes) {
+        Node minimum = null;
+        for (Node node : nodes) {
+            if (minimum == null) {
+                minimum = node;
+            } else if (getShortestDistance(node) < getShortestDistance(minimum)) {
+                minimum = node;
+            }
+        }
+        return minimum;
+    }
+
+    private void findMinimalDistances(Node node) {
+        List<Node> adjacentNodes = getNeighbors(node);
+        for (Node target : adjacentNodes) {
+            if (getShortestDistance(target) > getShortestDistance(node)
+                    + getDistance(node, target)) {
+                distance.put(target, getShortestDistance(node)
+                        + getDistance(node, target));
+                predecessors.put(target, node);
+                unSettledNodes.add(target);
+            }
+        }
 
     }
 
-    public PublicTransportRouting(Map<String, Integer> transportGraph) {
-        this.transportGraph = transportGraph;
+    private int getDistance(Node node, Node target) {
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(node)
+                    && edge.getDestination().equals(target)) {
+                return edge.getWeight();
+            }
+        }
+        throw new RuntimeException("Should not happen");
     }
-    
-    public void findGivenRoute(){
-        
+
+    private List<Node> getNeighbors(Node node) {
+        List<Node> neighbors = new ArrayList<Node>();
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(node)
+                    && !isSettled(edge.getDestination())) {
+                neighbors.add(edge.getDestination());
+            }
+        }
+        return neighbors;
     }
-    
-    public Map<String, Integer> getTransportGraph(){
+
+    private int getShortestDistance(Node destination) {
+        Integer d = distance.get(destination);
+        if (d == null) {
+            return Integer.MAX_VALUE;
+        } else {
+            return d;
+        }
+    }
+
+    private boolean isSettled(Node node) {
+        return settledNodes.contains(node);
+    }
+
+    public void findGivenRoute() {
+
+    }
+
+    public Map<String, Integer> getTransportGraph() {
         return transportGraph;
+    }
+
+    public LinkedList<Node> getPath(Node target) {
+        LinkedList<Node> path = new LinkedList<Node>();
+        Node step = target;
+        // check if a path exists
+        if (predecessors.get(step) == null) {
+            return null;
+        }
+        path.add(step);
+        while (predecessors.get(step) != null) {
+            step = predecessors.get(step);
+            path.add(step);
+        }
+        // Put it into the correct order
+        Collections.reverse(path);
+        return path;
     }
 }
--- src/test/java/com/here/ptr/PublicTransportRoutingTest.java
+++ src/test/java/com/here/ptr/PublicTransportRoutingTest.java
@@ -5,7 +5,13 @@
  */
 package com.here.ptr;
 
+import com.here.ptr.graph.domain.Edge;
+import com.here.ptr.graph.domain.Graph;
+import com.here.ptr.graph.domain.Node;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,6 +28,9 @@ public class PublicTransportRoutingTest {
 
     private PublicTransportRouting testRouteGraph;
 
+    private List<Node> nodes;
+    private List<Edge> edges;
+
     public PublicTransportRoutingTest() {
     }
 
@@ -35,14 +44,25 @@ public class PublicTransportRoutingTest {
 
     @Before
     public void setUp() {
-        Map<String, Integer> testRoute = new HashMap<String, Integer>();
-        testRoute.put("AB", 300);
-        testRoute.put("CB", 30);
-        testRoute.put("AD", 300);
-        testRoute.put("EF", 300);
-        testRoute.put("AC", 50);
-        testRoute.put("CA", 67);
-        testRouteGraph = new PublicTransportRouting(testRoute);
+        nodes = new ArrayList<Node>();
+        edges = new ArrayList<Edge>();
+        for (int i = 0; i < 11; i++) {
+            Node location = new Node("Node_" + i);
+            nodes.add(location);
+        }
+
+        addLane("Edge_0", 0, 1, 85);
+        addLane("Edge_1", 0, 2, 217);
+        addLane("Edge_2", 0, 4, 173);
+        addLane("Edge_3", 2, 6, 186);
+        addLane("Edge_4", 2, 7, 103);
+        addLane("Edge_5", 3, 7, 183);
+        addLane("Edge_6", 5, 8, 250);
+        addLane("Edge_7", 8, 9, 84);
+        addLane("Edge_8", 7, 9, 167);
+        addLane("Edge_9", 4, 9, 502);
+        addLane("Edge_10", 9, 10, 40);
+        addLane("Edge_11", 1, 10, 600);
     }
 
     @After
@@ -51,9 +71,22 @@ public class PublicTransportRoutingTest {
 
     @Test
     public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        assertTrue(testRouteGraph.getTransportGraph().size() >0);
-        testRouteGraph.findGivenRoute();
+        Graph graph = new Graph(nodes, edges);
+        PublicTransportRouting ptr = new PublicTransportRouting(graph);
+        ptr.runSrearch(nodes.get(0));
+        LinkedList<Node> path = ptr.getPath(nodes.get(10));
+
+        assertNotNull(path);
+        assertTrue(path.size() > 0);
+
+        for (Node node : path) {
+            System.out.println(node);
+        }
     }
 
+    private void addLane(String laneId, int sourceLocNo, int destLocNo,
+            int duration) {
+        Edge lane = new Edge(laneId, nodes.get(sourceLocNo), nodes.get(destLocNo), duration);
+        edges.add(lane);
+    }
 }
